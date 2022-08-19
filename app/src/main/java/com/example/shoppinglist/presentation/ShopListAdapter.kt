13c373760e6172e579f1e01a745554d6b6254023
DaniilElmirov <elmirovwork@gmail.com>
2022-08-19 14:15:10 +0700

    Change LinearLayout to RecyclerView

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -4,33 +4,64 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
 
-class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
-    val list = listOf<ShopItem>()
-
-    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
-        val tvItemName = view.findViewById<TextView>(R.id.tv_item_name)
-        val tvItemCount = view.findViewById<TextView>(R.id.tv_item_count)
-    }
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_disabled,
+            parent,
+            false
+        )
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
+        val shopItem = shopList[position]
 
-        holder.tvItemName.text = shopItem.name
-        holder.tvItemCount.text = shopItem.count.toString()
+        val status = if (shopItem.enable)
+            "Active"
+        else
+            "Not active"
 
         holder.view.setOnLongClickListener {
             true
         }
+
+        if (shopItem.enable) {
+            holder.tvItemName.text = "${shopItem.name} $status"
+            holder.tvItemCount.text = shopItem.count.toString()
+
+            holder.tvItemName.setTextColor(ContextCompat.getColor(
+                holder.view.context,
+                android.R.color.holo_red_light))
+        }
     }
 
-    override fun getItemCount() = list.size
+    override fun onViewRecycled(holder: ShopItemViewHolder) {
+        super.onViewRecycled(holder)
+
+        holder.tvItemName.text = ""
+        holder.tvItemCount.text = ""
+
+        holder.tvItemName.setTextColor(ContextCompat.getColor(
+            holder.view.context,
+            android.R.color.holo_red_light))
+    }
+
+    override fun getItemCount() = shopList.size
+
+    class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val tvItemName = view.findViewById<TextView>(R.id.tv_item_name)
+        val tvItemCount = view.findViewById<TextView>(R.id.tv_item_count)
+    }
 }
