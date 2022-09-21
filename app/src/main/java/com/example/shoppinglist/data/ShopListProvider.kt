015bd331649c12fdd947e7aca8cc42c1af41afdd
DaniilElmirov <elmirovwork@gmail.com>
2022-09-21 22:21:55 +0700

    Add UriMatcher in ShopListProvider.kt

--- app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
@@ -2,11 +2,18 @@ package com.example.shoppinglist.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShopListProvider: ContentProvider() {
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI("com.example.shoppinglist", "shop_items", GET_SHOP_ITEMS_QUERY)
+        addURI("com.example.shoppinglist", "shop_items/#", GET_SHOP_ITEMS_BY_ID_QUERY)    //вместо # в запросе может быть число
+        addURI("com.example.shoppinglist", "shop_items/*", GET_SHOP_ITEMS_BY_NAME_QUERY)  //вместо * в запросе может быть строка
+    }
+
     override fun onCreate() = true
 
     override fun query(
@@ -16,7 +23,13 @@ class ShopListProvider: ContentProvider() {
         p3: Array<out String>?,
         p4: String?,
     ): Cursor? {
-        Log.d("ShopListProvider", "query $p0")
+        val code = uriMatcher.match(p0)
+        when(code) {
+            GET_SHOP_ITEMS_QUERY -> {
+
+            }
+        }
+        Log.d("ShopListProvider", "query $p0 $code")
         return null
     }
 
@@ -34,5 +47,11 @@ class ShopListProvider: ContentProvider() {
 
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val GET_SHOP_ITEMS_QUERY = 100
+        private const val GET_SHOP_ITEMS_BY_ID_QUERY = 101
+        private const val GET_SHOP_ITEMS_BY_NAME_QUERY = 102
     }
 }
